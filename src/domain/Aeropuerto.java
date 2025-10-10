4e3a538b6c40bfdd5ea07cb669c601c0ab2c3814
Aimar1109 <aimar.junagore@gmail.com>
2025-10-10 09:03:39 +0200

    Aeropuerto atr, const, get y set

--- src/domain/Aeropuerto.java
+++ src/domain/Aeropuerto.java
@@ -1,7 +1,43 @@
 package domain;
 
 public class Aeropuerto {
+	
+	// ATRIBUTOS
 	private String codigo;
 	private String nombre;
 	private String ciudad;
+	
+	// CONSTRUCTOR
+	public Aeropuerto(String codigo, String nombre, String ciudad) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.ciudad = ciudad;
+	}
+	
+	// GETTERS Y SETTERS
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
 }
