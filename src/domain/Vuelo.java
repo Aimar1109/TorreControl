97a515304e7ccc23429f8314c9af7a36af358b8b
Aimar1109 <aimar.junagore@gmail.com>
2025-10-10 08:47:54 +0200

    Vuelo atributos, constructor, getters y setters

--- src/domain/Vuelo.java
+++ src/domain/Vuelo.java
@@ -1,7 +1,114 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class Vuelo {
 	
+	// ATRIBUTOS
+	private int codigo;
+	private Aeropuerto origen;
+	private Aeropuerto destino;
+	private Pista pista;
+	private PuertaEmbarque puerta;
+	private boolean estado;
+	private int duracion;
+	private Avion avion;
+	private boolean emergencia;
+	private ArrayList<String> pasajeros;
+	private ArrayList<String> tripulacion;
+	private int delayed;
 	
+	// CONSTRUCTOR	
+	public Vuelo(int codigo, Aeropuerto origen, Aeropuerto destino, Pista pista, PuertaEmbarque puerta, boolean estado,
+			int duracion, Avion avion, boolean emergencia, ArrayList<String> pasajeros, ArrayList<String> tripulacion,
+			int delayed) {
+		super();
+		this.codigo = codigo;
+		this.origen = origen;
+		this.destino = destino;
+		this.pista = pista;
+		this.puerta = puerta;
+		this.estado = estado;
+		this.duracion = duracion;
+		this.avion = avion;
+		this.emergencia = emergencia;
+		this.pasajeros = pasajeros;
+		this.tripulacion = tripulacion;
+		this.delayed = delayed;
+	}
+	
+	//GETTERS Y SETTERS
+	public int getcodigo() {
+		return codigo;
+	}
+	public void setcodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public Aeropuerto getOrigen() {
+		return origen;
+	}
+	public void setOrigen(Aeropuerto origen) {
+		this.origen = origen;
+	}
+	public Aeropuerto getDestino() {
+		return destino;
+	}
+	public void setDestino(Aeropuerto destino) {
+		this.destino = destino;
+	}
+	public Pista getPista() {
+		return pista;
+	}
+	public void setPista(Pista pista) {
+		this.pista = pista;
+	}
+	public PuertaEmbarque getPuerta() {
+		return puerta;
+	}
+	public void setPuerta(PuertaEmbarque puerta) {
+		this.puerta = puerta;
+	}
+	public boolean isEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	public Avion getAvion() {
+		return avion;
+	}
+	public void setAvion(Avion avion) {
+		this.avion = avion;
+	}
+	public boolean isEmergencia() {
+		return emergencia;
+	}
+	public void setEmergencia(boolean emergencia) {
+		this.emergencia = emergencia;
+	}
+	public ArrayList<String> getPasajeros() {
+		return pasajeros;
+	}
+	public void setPasajeros(ArrayList<String> pasajeros) {
+		this.pasajeros = pasajeros;
+	}
+	public ArrayList<String> getTripulacion() {
+		return tripulacion;
+	}
+	public void setTripulacion(ArrayList<String> tripulacion) {
+		this.tripulacion = tripulacion;
+	}
+	public int getDelayed() {
+		return delayed;
+	}
+	public void setDelayed(int delayed) {
+		this.delayed = delayed;
+	}
 	
 }
