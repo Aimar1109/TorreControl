f61532d12900ad65e545a74bd65ae5111717f181
Aimar1109 <aimar.junagore@gmail.com>
2025-10-12 21:41:46 +0200

    avion, pista, puerta nombre para interfaz vuelos

--- src/domain/Pista.java
+++ src/domain/Pista.java
@@ -1,5 +1,22 @@
 package domain;
 
 public class Pista {
+	
+	private String nombre;
+
+	public Pista(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 
 }
--- src/domain/PuertaEmbarque.java
+++ src/domain/PuertaEmbarque.java
@@ -1,5 +1,21 @@
 package domain;
 
 public class PuertaEmbarque {
+	
+	private String nombre;
 
+	public PuertaEmbarque(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 }
