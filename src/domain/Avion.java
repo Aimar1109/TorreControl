6389805e0ca5be4e36ca28dd74ce85dfb40f3838
OierAbrisqueta <oabrisketa76@gmail.com>
2025-10-30 20:03:12 +0100

    Comienzo con el dibujado del mapa y de los aviones

--- src/domain/Avion.java
+++ src/domain/Avion.java
@@ -1,5 +1,97 @@
 package domain;
 
+import java.awt.*;
+
 public class Avion {
 
+    private String modelo;
+    private String matricula;
+    private int capacidad;
+    private int x;
+    private int y;
+    private double angulo;
+    private Image imagen;
+    private int combustible;
+
+    public Avion() {
+        this.modelo = "";
+        this.matricula = "";
+        this.capacidad = 0;
+    }
+
+    public Avion(String modelo, String matricula, int capacidad) {
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.capacidad = capacidad;
+    }
+
+    public Avion(String modelo, String matricula, int capacidad, int x, int y, double angulo) {
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.capacidad = capacidad;
+        this.x = x;
+        this.y = y;
+        this.angulo = angulo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getAngulo() {
+        return angulo;
+    }
+
+    public void setAngulo(double angulo) {
+        this.angulo = angulo;
+    }
+
+    public Image getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(Image imagen) {
+        this.imagen = imagen;
+    }
+
+    public void mover(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
 }
--- src/gui/MapPanel.java
+++ src/gui/MapPanel.java
@@ -2,8 +2,12 @@ package gui;
 
 import domain.Avion;
 
+import javax.imageio.IIOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,8 +23,36 @@ public class MapPanel extends JPanel {
 
     public MapPanel() {
         super(null, true);
-        //this.avionesAeropuerto = avionesAeropuerto;
-        //this.mapaAeropuerto = mapaAeropuerto;
+        this.avionesAeropuerto = avionesAeropuerto;
+
+        cargarImagenMapa();
         setBackground(Color.BLACK);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        //Se activa antialiasing para mayor calidad
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        //Escalado para conseguir coordenadas relativas
+        double escaladoX = getWidth()/widthReal;
+        double escaladoY = getHeight()/heightReal;
+
+        //Dibuja mapa del aeropuerto
+        g2d.drawImage(mapaAeropuerto, 0, 0, getWidth(), getHeight(), this);
+
+        //Dibujo aviones únicamente si están en el mapa
+    }
+
+    private void cargarImagenMapa() {
+        try {
+            mapaAeropuerto = ImageIO.read(new File("/resources/img/aeropuerto.PNG"));
+        } catch (IOException e) {
+            //En caso de no poder cargarlo el fondo se queda negro
+            System.err.println("No se ha podido cargar el mapa" + e.getMessage());
+        }
     }
 }
