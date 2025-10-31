c26c4c2496909f7a049a472c82afac2755aaa1a9
jonsacha <jonsacha.diez@opendeusto.es>
2025-10-31 08:28:34 +0100

    prueba push

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,6 +17,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("hola");
+		
 		ArrayList<Vuelo> vuelos = new ArrayList<Vuelo>();
 		
 		Avion av = new Avion("a1");
